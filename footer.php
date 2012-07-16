d23f456211274345eebf1e74154ddf1a3385b455
Vincent Boiardt <vincent@boiardt.se>
2012-07-16 23:38:13 +0200

    More styling

---
+++ footer.php
@@ -0,0 +1,9 @@
+	</div>
+	<footer>
+		<div class="inner">
+			<p class="copyright"><?php echo esc_attr( get_option( 'bb_footer' ) ); ?></p>
+		</div>
+	</footer>
+	<?php wp_footer(); ?>
+</body>
+</html>
--- home.php
+++ home.php
@@ -3,7 +3,8 @@
 	<?php while ( have_posts() ) : the_post(); ?>
 		<h1><?php the_title(); ?></h1>
 		<?php the_content(); ?>
-	<?php endwhile; ?></section>
+	<?php endwhile; ?>
+</section>
 <aside id="sidebar">
 	<article class="module">
 		<h2>En module</h2>
